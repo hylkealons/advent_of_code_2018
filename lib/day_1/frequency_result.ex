709dc4e92afcb62ce46f0cf5a5024bc4c1531215
Hylke Alons <halons@digitalorigin.com>
2018-12-01 21:45:20 +0100

    Day 1

---
+++ lib/day_1/frequency_result.ex
@@ -0,0 +1,54 @@
+defmodule AdventOfCode.ResultingFrequency do
+  @moduledoc """
+  URL: https://adventofcode.com/2018/day/1#part2
+  """
+
+  @typedoc """
+  The frequency list exists of a frequencies as a string, e.g. ["-16", "+15"]
+  """
+  @type frequency_list :: [String.t()]
+
+  @doc """
+  Calculates the frequency based on a list of frequencies
+  """
+  @spec calculate(frequency_list) :: integer
+  def calculate(frequency_list) do
+    Enum.reduce(frequency_list, 0, &calculate_frequency(&2, &1))
+  end
+
+  @doc """
+  Finds the first duplicated frequency result
+  """
+  @spec first_frequency(frequency_list, frequency_list) :: integer
+  def first_frequency(frequency_list, result_frequencies \\ [0]) do
+    case find_first_frequency(frequency_list, result_frequencies) do
+      {:found, frequency} -> frequency
+      {:not_found, result_frequencies} -> first_frequency(frequency_list, result_frequencies)
+    end
+  end
+
+  defp find_first_frequency([], result_frequencies), do: {:not_found, result_frequencies}
+
+  defp find_first_frequency([frequency | left], [current_frequency | _] = result_frequencies) do
+    resulting_frequency = calculate_frequency(current_frequency, frequency)
+
+    if Enum.member?(result_frequencies, resulting_frequency) do
+      {:found, resulting_frequency}
+    else
+      find_first_frequency(left, [resulting_frequency | result_frequencies])
+    end
+  end
+
+  defp calculate_frequency(current, "+" <> frequency) do
+    current + frequency_to_integer(frequency)
+  end
+
+  defp calculate_frequency(current, "-" <> frequency) do
+    current - frequency_to_integer(frequency)
+  end
+
+  defp frequency_to_integer(frequency) do
+    {frequency, _} = Integer.parse(frequency)
+    frequency
+  end
+end
---
+++ test/day_1/frequency_result_test.exs
@@ -0,0 +1,47 @@
+defmodule AdventOfCode.ResultingFrequencyTest do
+  use ExUnit.Case
+
+  alias AdventOfCode.ResultingFrequency
+
+  @frequencies "test/day_1/fixtures/input.txt" |> File.stream!() |> Enum.map(& &1)
+
+  describe "calculate/1" do
+    test "sums correctly" do
+      assert ResultingFrequency.calculate(["+1", "+1", "+1"]) == 3
+    end
+
+    test "subtracts correctly" do
+      assert ResultingFrequency.calculate(["-1", "-2", "-3"]) == -6
+    end
+
+    test "calculates correctly" do
+      assert ResultingFrequency.calculate(["+1", "+1", "-2"]) == 0
+    end
+
+    test "calculate for frequency input" do
+      assert ResultingFrequency.calculate(@frequencies) == 439
+    end
+  end
+
+  describe "first_frequency" do
+    test "first 0 twice" do
+      assert ResultingFrequency.first_frequency(["+1", "-1"]) == 0
+    end
+
+    test "reaches 10 twice" do
+      assert ResultingFrequency.first_frequency(["+3", "+3", "+4", "-2", "-4"]) == 10
+    end
+
+    test "reaches 5 twice" do
+      assert ResultingFrequency.first_frequency(["-6", "+3", "+8", "+5", "-6"]) == 5
+    end
+
+    test "reaches 14 twice" do
+      assert ResultingFrequency.first_frequency(["+7", "+7", "-2", "-7", "-4"]) == 14
+    end
+
+    test "find for frequency input" do
+      assert ResultingFrequency.first_frequency(@frequencies) == 124_645
+    end
+  end
+end
